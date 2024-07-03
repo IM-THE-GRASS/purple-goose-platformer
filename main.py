c89aa30e2ac7ae694f2722f79043dafb98e38f4e
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-07-03 14:27:40 -0600

    buttons

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
-import  time
+import os
 click = False
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 pygame.init()
-font = pygame.font.SysFont("times new roman", 32)
+font = pygame.font.SysFont("times new roman", 32, bold=True)
 running = True
 screen_width = 700
 screen_height = 700
@@ -12,23 +12,34 @@ screen = pygame.display.set_mode((700, 700))
 pygame.display.set_caption("goobse")
 
 class button:   
-    def __init__(self, centerx, centery, width, height, text, bg_color = WHITE, text_color = BLACK):
+    def __init__(self, centerx, centery, width, height, type = "text", text = "", image_path= "", bg_color = WHITE, text_color = BLACK):
         self.x = centerx 
         self.y = centery
         self.width = width
         self.height = height
         self.text = text
+        self.bg_color = bg_color
+        self.text_color = text_color
+        self.type = type
+        self.image_path = image_path
         
-        self.rect = pygame.rect(0,0,0,0)
+        self.rect = pygame.Rect(0,0,0,0)
         self.rect.center = (centerx,centery)
         self.rect.width = width
         self.rect.height = height
-    def draw(self, text, x, y):
-        text_surface = font.render(text, True, BLACK)
-        screen.blit(text_surface, (x - text_surface.get_width() / 2, y - text_surface.get_height() / 2))
+    def draw(self, click_function):
+        pygame.draw.rect(screen, self.bg_color, self.rect)
+        if self.type == "text":
+            surface = font.render(self.text, False, BLACK)
+        elif self.type == "image":
+            surface = pygame.image.load(self.image_path)
+            surface = pygame.transform.scale(surface, (self.width,self.height))
+        screen.blit(surface, (self.x, self.y))
+        if self.rect.collidepoint(mouse_x, mouse_y) and click:
+            click_function()
+            
 
 while running:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -39,21 +50,22 @@ while running:
             click = False
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    play_button = button(50, 100, 200, 50)
-    quit_button = button(50, 200, 200, 50)
+    def on_play():
+        print("play")
+        pygame.quit()
+        
+    def on_quit():
+        print("quit")
+        pygame.quit()
+    
+    
+    play_button = button(25, 25, 500, 125, "image", image_path=os.path.join('images', 'play.png'))
+    quit_button = button(25, 150, 500, 125, "image", image_path=os.path.join('images', 'quit.png'))
+    
 
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
-        if click:
-            running = False
 
-    if quit_button.rect.collidepoint(mouse_x, mouse_y):
-        if click:
-            running = False
-    screen.fill(BLACK)
-    pygame.draw.rect(screen, WHITE, play_button_rect)
-    draw_text("Play Game", play_button_rect.centerx, play_button_rect.centery)
 
-    pygame.draw.rect(screen, WHITE, quit_button_rect)
-    draw_text("Quit", quit_button_rect.centerx, quit_button_rect.centery)
+    play_button.draw(on_play)
+    quit_button.draw(on_quit)
 
     pygame.display.flip()
