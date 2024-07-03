b79629dcff853348354e72866a4944fe1460ed94
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-07-03 11:24:59 -0600

    button class

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ screen_height = 700
 screen = pygame.display.set_mode((700, 700))
 pygame.display.set_caption("goobse")
 
-
-    
-def draw_text(text, x, y):
-  text_surface = font.render(text, True, BLACK)
-  screen.blit(text_surface, (x - text_surface.get_width() / 2, y - text_surface.get_height() / 2))
+class button:   
+    def __init__(self, centerx, centery, width, height, text, bg_color = WHITE, text_color = BLACK):
+        self.x = centerx 
+        self.y = centery
+        self.width = width
+        self.height = height
+        self.text = text
+        
+        self.rect = pygame.rect(0,0,0,0)
+        self.rect.center = (centerx,centery)
+        self.rect.width = width
+        self.rect.height = height
+    def draw(self, text, x, y):
+        text_surface = font.render(text, True, BLACK)
+        screen.blit(text_surface, (x - text_surface.get_width() / 2, y - text_surface.get_height() / 2))
 
 while running:
     
@@ -29,14 +39,14 @@ while running:
             click = False
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    play_button_rect = pygame.Rect(50, 100, 200, 50)
-    quit_button_rect = pygame.Rect(50, 200, 200, 50)
+    play_button = button(50, 100, 200, 50)
+    quit_button = button(50, 200, 200, 50)
 
-    if play_button_rect.collidepoint(mouse_x, mouse_y):
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
         if click:
             running = False
 
-    if quit_button_rect.collidepoint(mouse_x, mouse_y):
+    if quit_button.rect.collidepoint(mouse_x, mouse_y):
         if click:
             running = False
     screen.fill(BLACK)
