a50ceed93591fa72f9653b55f5095938ed7930b8
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-07-03 18:07:34 -0600

    menu

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import pygame
 import os
 import ui
-click = False
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 pygame.init()
+click = False
+mouse_pos = pygame.mouse.get_pos()
+mouse_info = [click, mouse_pos]
 font = pygame.font.SysFont("times new roman", 32, bold=True)
 running = True
 screen_width = 700
 screen_height = 700
 screen = pygame.display.set_mode((1200, 700))
 pygame.display.set_caption("goobse")
+def on_play():
+    print("play")
+    pygame.quit()
+    
+def on_quit():
+    print("quit")
+    pygame.quit()
 
+def on_settings():
+    print("settings")
 
-            
-
+menu = ui.menu(click, mouse_pos,screen,(25, 25))
+menu.add_button(500, 125,"image",os.path.join('images', 'play.png'),"play", on_play)
+menu.add_button(500, 125,"image",os.path.join('images', 'settings.png'),"settings", on_play)
+menu.add_button(500, 125,"image",os.path.join('images', 'quit.png'),"quit", on_play)
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -25,28 +40,21 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             click = False
     mouse_pos = pygame.mouse.get_pos()
+    menu.set_mouse_info(click, mouse_pos)
 
-    def on_play():
-        print("play")
-        pygame.quit()
-        
-    def on_quit():
-        print("quit")
-        pygame.quit()
     
-    def on_settings():
-        print("settings")
     
     screen.fill(BLACK)
     
-    play_button = ui.button(25, 25, 500, 125, "image", image_path=os.path.join('images', 'play.png'))
-    settings_button = ui.button(25, 150, 500, 125, "image", image_path=os.path.join("images", "settings.png"))
-    quit_button = ui.button(25, 275, 500, 125, "image", image_path=os.path.join('images', 'quit.png'))
+    #play_button = ui.button(25, 25, 500, 125, "image", image_path=os.path.join('images', 'play.png'))
+    #settings_button = ui.button(25, 150, 500, 125, "image", image_path=os.path.join("images", "settings.png"))
+    #quit_button = ui.button(25, 275, 500, 125, "image", image_path=os.path.join('images', 'quit.png'))
+    
+    menu.draw()
     
 
-
-    play_button.draw(on_play, click, screen, mouse_pos)
-    settings_button.draw(on_settings, click, screen, mouse_pos)
-    quit_button.draw(on_quit, click, screen, mouse_pos)
+    #play_button.draw(on_play, click, screen, mouse_pos)
+    #settings_button.draw(on_settings, click, screen, mouse_pos)
+    #quit_button.draw(on_quit, click, screen, mouse_pos)
 
     pygame.display.flip()
--- ui.py
+++ ui.py
@@ -5,7 +5,7 @@ font = pygame.font.SysFont("times new roman", 32, bold=True)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 class button:   
-    def __init__(self, centerx, centery, width, height, type = "text", text = "", image_path= "", bg_color = WHITE, text_color = BLACK):
+    def __init__(self, centerx, centery, width, height, type = "text",image_path= "", text = "", bg_color = WHITE, text_color = BLACK):
         pygame.init()
         self.x = centerx 
         self.y = centery
@@ -22,6 +22,7 @@ class button:
         self.rect.width = width
         self.rect.height = height
     def draw(self, click_function, click_value, screen, mouse_pos):
+        print(click_value)
         pygame.draw.rect(screen, self.bg_color, self.rect)
         if self.type == "text":
             surface = font.render(self.text, False, BLACK)
@@ -32,6 +33,30 @@ class button:
             surface.convert_alpha()
             surface.set_colorkey((255,255,255))
         if self.rect.collidepoint(mouse_pos) and click_value:
+            print("hi")
             click_function()
+            
         screen.blit(surface, (self.x, self.y))
         return surface, (self.x, self.y)
+class menu:
+    def __init__(self, mouse_down, mouse_pos, screen, starting_pos):
+        self.mouse_pos = mouse_pos
+        self.mouse_down = mouse_down
+        self.screen = screen
+        self.bg = None
+        self.gap = 125
+        self.starting_pos = starting_pos
+        self.buttons = []
+    
+    def add_button(self, width, height, type, path, name, on_click):
+        the_button = button(self.starting_pos[0],self.starting_pos[1] + self.gap * len(self.buttons), width, height, type, path)
+        self.buttons.append({"button": the_button, "on_click":on_click, "name": name})
+    def remove_button(self):
+        pass
+    def set_mouse_info(self, mouse_down, mouse_pos):
+        self.mouse_down = mouse_down
+        self.mouse_pos = mouse_pos
+    def draw(self):
+        print(self.mouse_pos)
+        for thebutton in self.buttons:
+            thebutton["button"].draw(thebutton["on_click"], self.mouse_down, self.screen, self.mouse_pos)
