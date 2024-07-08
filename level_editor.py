73a5e3bb0ce0f4f278070639bbc95ef1afa45e5e
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-07-07 19:52:58 -0600

    it amkes a preview of the blocks before yopu place them now

--- level_editor.py
+++ level_editor.py
@@ -15,8 +15,23 @@ def main(level):
     platforms = pygame.sprite.Group()
     clock = pygame.time.Clock()
     current_block = {}
+    
+    def generate_block(startpos, endpos):
+        width =  endpos[0] - startpos[0]
+        height = endpos[1] - startpos[1]
+        x = startpos[0]
+        y = startpos[1]
+        if width < 0:
+            x = x + width
+            width = width * -1
+            
+        if height < 0:
+            y = y + height
+            height = height * -1
+        return width, height, x, y
     while running:
         clock.tick(60)
+        mouse_pos = pygame.mouse.get_pos() 
         click = None
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -27,32 +42,26 @@ def main(level):
             if event.type == pygame.MOUSEBUTTONUP:
                 if current_block:
                     endpos = mouse_pos
-                    current_block["end_pos"] = endpos
                     startpos = current_block["start_pos"]
-                    width =  endpos[0] - startpos[0]
-                    height = endpos[1] - startpos[1]
-                    x = startpos[0]
-                    y = startpos[1]
-                    if width < 0:
-                        x = x + width
-                        width = width * -1
-                        
-                    if height < 0:
-                        y = y + height
-                        height = height * -1
-                        
-                        
-                    
-                    
+                    current_block["end_pos"] = endpos
+                    width, height, x, y = generate_block(startpos, endpos)
                     platform = block.Block(WHITE,width,height,x,y)
                     platforms.add(platform)
+                    current_block = {}
             
-        mouse_pos = pygame.mouse.get_pos()        
+               
             
         screen.fill(BLACK)
-
-        
-        
+        print(not "endpos" in current_block.keys())
+        print(current_block.keys())
+        if not "end_pos" in current_block.keys() and "start_pos" in current_block.keys():
+            print("ADDA")
+            width, height, x, y = generate_block(current_block["start_pos"], mouse_pos)
+            s = pygame.Surface((width,height))
+            s.set_alpha(128)
+            s.fill((255,255,255))
+            screen.blit(s, (x,y))
+                    
         platforms.update()
         platforms.draw(screen)
         pygame.display.flip()
