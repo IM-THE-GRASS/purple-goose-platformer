f5ce344620d150ba9da73a2dd521aea7cfb32580
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-07-13 14:28:48 -0600

    cleaned up my code but ended up breaking something and i also added a type feild into the level data so i can make it into a finish flag

--- level_editor.py
+++ level_editor.py
@@ -11,41 +11,56 @@ current_tool = "draw"
 
 
 def main(level):
-    global paused 
-    paused = False
+    def get_level_data(lvl):
+        f = open(level)
+        level_data = f.read()
+        f.close()
+        level_data = json.loads(level_data)
+        return level_data
+    def open_menu(_ = None):
+        level_editor.running = False
+        main_menu.main()  
+    def exit(_ = None):
+        pygame.quit()
+        quit()
+    
+    # pygame stuff
     BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
     pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((1200, 700))
+    
+    
+    # fisics
     space = pymunk.Space()
     space.gravity = (0,0)
+    
+    
+    #ui 
     cp = ui.colorpicker(50, 50, 400, 60)
     delete = ui.button(50,150,70,70,"image", os.path.join("images", "delete.png"))
     draw = ui.button(50,230,70,70,"image", os.path.join("images", "draw.png"))
     finish_button = ui.button(50,310,70,70,"image", os.path.join("images", "finish.png"))
-    click = False
-    font = pygame.font.SysFont("times new roman", 32, bold=True)
-    running = True
-    screen = pygame.display.set_mode((1200, 700))
-    f = open(level)
-    level_data = f.read()
-    f.close()
-    level_data = json.loads(level_data)
-    platforms = []
-    clock = pygame.time.Clock()
-    current_block = {}
-    def menu(_ = None):
-        level_editor.running = False
-        main_menu.main()
-        
-    def end(_ = None):
-        pygame.quit()
-        quit()
     pause_screen = pygame.image.load(os.path.join("images", "paused.png")).convert_alpha()
     pause_button = ui.button(1090, 10, 100,100,"image",os.path.join('images', 'pause.png'),sound_path=os.path.join("sounds", "menu_select.wav"))
     main_menu_button = ui.button(300,341,573,142,"image",image_path=os.path.join('images', 'main menu.png'),sound_path=os.path.join("sounds", "menu_select.wav"))
     quit_button = ui.button(300,490,573,142,"image",image_path=os.path.join('images', 'quit.png'),sound_path=os.path.join("sounds", "menu_select.wav"))
-    for box in level_data:
-        platforms.append(block.Block(box["color"],box["width"],box["height"],box["x"],box["y"], space))
+    
+    # variables to keep track of stuff
+    global paused 
+    paused = False
+    click = False
+    running = True
+    
+    
+    # level data stuff
+    level_data = get_level_data(level)
+    platforms = []
+    current_block = {}
+    def load_data():
+        for box in level_data:
+            platforms.append(block.Block(box["color"],box["width"],box["height"],box["x"],box["y"], space))
+    load_data()
     def generate_block(startpos, endpos):
         width =  endpos[0] - startpos[0]
         height = endpos[1] - startpos[1]
@@ -59,8 +74,10 @@ def main(level):
             y = y + height
             height = height * -1
         return width, height, x, y
+    
     while running:
-        color = cp.get_color()
+        print(current_block)
+        current_color = cp.get_color()
         clock.tick(60)
         mouse_pos = pygame.mouse.get_pos() 
         click = None
@@ -72,30 +89,27 @@ def main(level):
                 click = "down"
                 if current_tool == "draw":
                     current_block["start_pos"] = mouse_pos
-                    
-            if event.type == pygame.MOUSEBUTTONUP:
-                click = "up"
+                    print("AAA")
                 if current_tool == "draw":
                     if current_block:
                         endpos = mouse_pos
                         startpos = current_block["start_pos"]
                         current_block["end_pos"] = endpos
                         width, height, x, y = generate_block(startpos, endpos)
-                        platforms.append(block.Block(color,width,height,x,y, space))
+                        platforms.append(block.Block(current_color,width,height,x,y, space))
 
-                        rec = {"x":x,"y":y,"width":width,"height":height, "color":(color.r,color.g,color.b)}
+                        rec = {"x":x,"y":y,"width":width,"height":height, "color":tuple(current_color), "type":"platform"}
                         level_data.append(rec)
                         
                         current_block = {}
                 elif current_tool == "delete":
-                    
                     for bloc in platforms:
-                        print({"x":bloc.x,"y":bloc.y,"width":bloc.width,"height":bloc.height, "color":bloc.color})
+
                         rect = pygame.Rect(bloc.x,bloc.y,bloc.width,bloc.height)
                         if rect.collidepoint(mouse_pos):
                             platforms.remove(bloc)
                             for rec in level_data:
-                                if rec == {"x":bloc.x,"y":bloc.y,"width":bloc.width,"height":bloc.height, "color":bloc.color}:
+                                if rec == {"x":bloc.x,"y":bloc.y,"width":bloc.width,"height":bloc.height, "color":bloc.color, "type":"platform"}:
                                     print(rec)
                                     level_data.remove(rec)
 
@@ -131,8 +145,8 @@ def main(level):
             pause_button.draw(level_editor.pause, click, screen, mouse_pos)
         if paused:
             screen.blit(pause_screen, (0,0)) 
-            main_menu_button.draw(menu, click, screen, mouse_pos)
-            quit_button.draw(end, click, screen, mouse_pos)
+            main_menu_button.draw(open_menu, click, screen, mouse_pos)
+            quit_button.draw(exit, click, screen, mouse_pos)
         pygame.display.flip()
             
         
